8ad8c664423312b4b70974b6dd73a12d85c73677
j154370 <lihyo15@gmail.com>
2024-09-13 09:26:13 +0900

    aa

--- src/test/java/com/goodee/app/members/MemberServiceTest.java
+++ src/test/java/com/goodee/app/members/MemberServiceTest.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
+@Slf4j
 class MemberServiceTest {
 
 	@Autowired
@@ -17,12 +20,33 @@ class MemberServiceTest {
 	private PasswordEncoder passwordEncoder;
 	
 	@Test
-	void test() throws Exception{
+	void passwordUpdateTest() throws Exception{
+		// admin, 123123 -> admin
 		MemberVo memberVo = new MemberVo();
 		memberVo.setUsername("admin");
-		memberVo.setPassword(passwordEncoder.encode("123123"));
-		int result = memberMapper.pwUpdate(memberVo);
-		assertEquals(1, result);
+		memberVo.setPassword("123123");
+		String newpassword="admin";
+		
+		MemberVo check = memberMapper.detail(memberVo);
+		
+		log.info("MemberVO : {}", memberVo);
+		log.info("check : {}", check);
+				
+		if(passwordEncoder.matches(memberVo.getPassword(), check.getPassword())) {
+			log.info("===============성공=============");
+		}
+		
+		assertEquals(memberVo.getPassword(), check.getPassword());
 	}
+	
+	
+//	@Test
+//	void test() throws Exception{
+//		MemberVo memberVo = new MemberVo();
+//		memberVo.setUsername("admin");
+//		memberVo.setPassword(passwordEncoder.encode("123123"));
+//		int result = memberMapper.pwUpdate(memberVo);
+//		assertEquals(1, result);
+//	}
 
 }
